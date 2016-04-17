71fdd1339fbcfdbd89274399b521f0c86193c19e
yuma <yuma-o.soccer-919451@ezweb.ne.jp>
2016-04-17 06:08:48 +0000

    maketable

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
 
-  get 'lists' => 'lists#index'
+  get '/lists' => 'lists#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
